8890f9a1edda499e127c8e6c33a1ebed1b1ab6d9
Matt <mattmartino54@gmail.com>
2022-07-22 13:55:10 -0500

    Updated to auto input fishlist

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,25 +1,26 @@
 // Import the function that returns a copy of the fish array
-import { } from ''
-import { getFish } from "./database"
+import { getFish } from './database.js'
 
 export const FishList = () => {
     // Invoke the function that you imported from the database module
     const fishes = getFish()
 
     // Start building a string filled with HTML syntax
-    const htmlString = '<article class="fishList">'
+    const htmlString = '<article class="masterFishList">'
 
     // Create HTNL representations of each fish here
     for (const fish of fishes) {
 
-        // Why is there a backtick used for this string? Cause the ${} property
-        htmlString += `<section class="fish card">
+        // Why is there a backtick used for this string? -Because we are linking a specific value in the object.
+        htmlString += `<section class="fish_card">
             <div><img  class="fish__image image--card" src="${fish.image}" /></div>
             <div class="fish__name">${fish.name}</div>
             <div class="fish__species">${fish.species}</div>
             <div class="fish__length">${fish.length}</div>
             <div class="fish__location">${fish.location}</div>
             <div class="fish__diet">${fish.food}</div>
+            <div class="keeping">${fish.Keeping}</div>
+            <div class="travel__tips">${fish.TravelTips}</div>
         </section>
 `
     }
@@ -27,18 +28,3 @@ export const FishList = () => {
 
     return htmlString
 }
-
-// Import the FishList function from the correct module
-import { } from ''
-
-/*
-    What is the CSS selector for the element where you
-    want to display the fish?
-
-    Use . for elements with a "class" attribute
-    Use # for elements with an "id" attribute
- */
-const parentHTMLElement = document.querySelector("insert selector here")
-
-
-parentHTMLElement.innerHTML = FishList()
--- scripts/database.js
+++ scripts/database.js
@@ -1,16 +1,78 @@
+export const getFish = () => {
+    return database.fish.map(fish => ({ ...fish }))
+}
 
 const database = {
     fish: [
         {
             name: "Bart",
+            image: "https://miarrecife.com/wp-content/uploads/2020/07/clownfish-1-scaled.jpg",
+            species: "Clown Fish",
             food: "crustaceans",
-            sizae: 3
+            length: 3,
+            location: "Gulf of Mexico",
+            TravelTips: "Don't eat the local fish!  Fish are friends not food",
+            Keeping: "Likes clean salty water at a temp of 83F"
 
-        }
+        }, 
+        {
+            name: "Pink Terror",
+            image: "https://www.buildyouraquarium.com/wp-content/uploads/2020/02/kole-tang.jpg",
+            species: "Pink Kale Tang",
+            food: "Fish",
+            length: 6,
+            location: "Haiti",
+            TravelTips: "Don't upset the locals",
+            Keeping: "Likes clean water, temps around 75-85F, and salty ass water."
+        },
+        {
+            name: "Peppermint",
+            igame: "https://treasuresoftheseaandmore.com/wp-content/uploads/2017/06/fish1.jpg",
+            species: "Pomascus",
+            food: "Kelp and little bugs",
+            length: 2.5,
+            location: "Dominican Republic",
+            TravelTips: "Visit Peurto Plata",
+            Keeping: "Likes clean water, temps around 75-85F, and salty ass water."
+        },
+        {
+            name: "Big Buddah",
+            image: "https://1.bp.blogspot.com/-EImP9NrxXNo/XtkrOyu6PVI/AAAAAAAAC6k/UJUefitWLLgSxoC9AlKxOxdhvLpZi0zfACNcBGAsYHQ/w1200-h630-p-k-no-nu/Untitled-1.webp",
+            species: "Yellow Tang",
+            food: "aquatic bugs",
+            length: 2,
+            location: "Keys of Florida",
+            TravelTips: "Ask for Dive Master Steve",
+            Keeping: "Likes clean water, temps around 75-85F, and salty ass water."
+        },
+        {
+            name: "Pissed off Pete",
+            image: "https://images1.ratemyfishtank.com/photo/13/860x640h/38000/37863/16239_2.jpg",
+            species: "Yellow blenny",
+            food: "small crabs",
+            length: 4,
+            location: "Bahamas",
+            TravelTips: "Get a drink from The Frosty Fish",
+            Keeping: "Likes clean water, temps around 75-85F, and salty ass water."
+        },
+        {
+            name: "Patty Mayonnaise",
+            image: "https://www.buildyouraquarium.com/wp-content/uploads/2020/02/dwarf-lionfish.jpg",
+            species: "Blenny",
+            food: "small crabs and crunchy bugs",
+            length: 3.5,
+            location: "coast of Florida",
+            TravelTips: "Take the bus for half price.",
+            Keeping: "Likes clean water, temps around 75-85F, and salty ass water."
+        },  
+
     ]
 }
+/*
+What is the data type of the database variable?  -An object with an array full of objects inside it.
+What is the data type of the fish property?      -An array of fish objects
+What is the data type of the name property?      -A string with a name in it.
+What is the data type of the size property?      -An intiger 
+What data types will the fish array contain?     -3x strings and one intiger.  
+*/
 
-
-export const getFish = () => {
-    return database.fish.map(fish => ({ ...fish }))
-}
